e0d0532f6ff7ccfebaaba1d70270f48a97359b04
Abel Sánchez <abelsandev@gmail.com>
2024-12-04 07:45:38 -0700

    pokedex finished

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -10,12 +10,17 @@ import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
   constructor(
     @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
-  ) {}
+
+    private readonly configService: ConfigService,
+  ) {
+    console.log(process.env.PORT);
+  }
   async create(createPokemonDto: CreatePokemonDto) {
     try {
       const pokemon = await this.pokemonModel.create(createPokemonDto);
